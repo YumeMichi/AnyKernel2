d70918d3f0be797e62afff0e3c5bf8fcc184eb24
YumeMichi <do4suki@gmail.com>
2019-04-15 16:10:46 +0800

    AK3: Add PolarKernel support
    Change-Id: I5b7b5d3098f6bcffbe4c5c3c43b66cfaf6474e40
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -2,63 +2,127 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# begin properties
+# Begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=PolarKernel by YumeMichi @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=dipper
+supported.versions=10
 supported.patchlevels=
-'; } # end properties
+'; } # End properties
 
-# shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+# Shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
+# Import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
+## Begin vendor changes
+mount -o rw,remount -t auto /vendor > /dev/null;
+
+cp -rf $home/patch/init.polar.sh /vendor/etc/init/hw/;
+chmod 0644 /vendor/etc/init/hw/init.polar.sh;
+
+# Make a backup of init.target.rc
+restore_file /vendor/etc/init/hw/init.target.rc;
+backup_file /vendor/etc/init/hw/init.target.rc;
+
+# Do work #2
+replace_string /vendor/etc/init/hw/init.target.rc "write /dev/stune/top-app/schedtune.colocate 0" "write /dev/stune/top-app/schedtune.colocate 1" "write /dev/stune/top-app/schedtune.colocate 0";
+
+# Add performance tweaks
+append_file /vendor/etc/init/hw/init.target.rc "==== PolarKernel ====" init.target.rc;
+
+# Make a backup of msm_irqbalance.conf
+restore_file /vendor/etc/msm_irqbalance.conf;
+backup_file /vendor/etc/msm_irqbalance.conf;
+
+cp -rf $home/patch/msm_irqbalance.conf /vendor/etc/msm_irqbalance.conf;
+chmod 0644 /vendor/etc/msm_irqbalance.conf;
+
+# Make a backup of fstab.qcom
+restore_file /vendor/etc/fstab.qcom;
+backup_file /vendor/etc/fstab.qcom;
+
+cp -rf $home/patch/fstab.qcom /vendor/etc/fstab.qcom;
+chmod 0644 /vendor/etc/fstab.qcom;
+
+# Make a backup of /vendor/etc/thermal-engine*
+for files in /vendor/etc/thermal-engine*
+do
+  restore_file $files;
+  backup_file $files;
+  $bb cat /dev/null > $files;
+done
 
 ## AnyKernel install
+ui_print " " "Decompressing boot image..."
 dump_boot;
 
-# begin ramdisk changes
+# Begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# Optimize F2FS extension list (@arter97)
+find /sys/fs -name extension_list | while read list; do
+  if grep -q odex "$list"; then
+    echo "Extensions list up-to-date: $list"
+    continue
+  fi
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+  echo "Updating extension list: $list..."
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+  echo "Clearing extension list..."
 
-# end ramdisk changes
+  HOT=$(cat $list | grep -n 'hot file extens' | cut -d : -f 1)
+  COLD=$(($(cat $list | wc -l) - $HOT))
 
+  COLDLIST=$(head -n$(($HOT - 1)) $list | grep -v ':')
+  HOTLIST=$(tail -n$COLD $list)
+
+  echo $COLDLIST | tr ' ' '\n' | while read cold; do
+    if [ ! -z $cold ]; then
+      echo "[c]!$cold" > $list
+    fi
+  done
+
+  echo $HOTLIST | tr ' ' '\n' | while read hot; do
+    if [ ! -z $hot ]; then
+      echo "[h]!$hot" > $list
+    fi
+  done
+
+  echo "Writing new extension list..."
+
+  cat $home/f2fs-cold.list | grep -v '#' | while read cold; do
+    if [ ! -z $cold ]; then
+      echo "[c]$cold" > $list
+    fi
+  done
+
+  cat $home/f2fs-hot.list | while read hot; do
+    if [ ! -z $hot ]; then
+      echo "[h]$hot" > $list
+    fi
+  done
+done
+
+# End ramdisk changes
+
+ui_print " " "Installing new boot image..."
 write_boot;
-## end install
 
+## End install
+ui_print " " "Done!"
+$bb umount /system
+$bb umount /vendor
